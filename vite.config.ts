c5b5ebac45e21b7e43614cab368ade9663f17cce
Jonas <jonas.rodriguez.external@eviden.com>
2025-10-05 11:05:18 +0200

    New vite config to proxy the /api calls

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   define: {
     __APP_VERSION__: JSON.stringify(version),
   },
 })
+ 
